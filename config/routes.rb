e23821613844143714fdb734b19f18344b4158b9
Andrew Erlanger <a.erlanger@gmail.com>
2019-01-14 10:11:53 +0100

    add contact and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :poems
+  get '/about', to: 'pages#about'
+  get '/contact', to: 'pages#contact'
 end
